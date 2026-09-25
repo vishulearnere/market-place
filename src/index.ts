074ecc2310d11bd115e851f6d04db0fbbac2ab9a
vishulearnere <shivharevishal14@gmail.com>
2026-09-25 17:03:49 +0530

    added swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
 import 'dotenv/config'
-import express, { Request, Response } from 'express'
+import express, { Request, Response, json } from 'express'
+import path from 'path'
 import user from './routes/v1/user.js'
 import product from './routes/v1/products.js'
 import shop from './routes/v1/shop.js'
 import { prisma } from './lib/prisma.js'
 // import {Prisma} from '@prisma/client'
 import bodyParser from 'body-parser'
+
+
+import helmet from 'helmet'
+import rateLimiter from 'express-rate-limit'
+import cors from 'cors'
+import xss from 'xss-clean'
+
+
+import swaggerUI from 'swagger-ui-express'
+import YAML from 'yamljs'
+
+const swaggerPath = path.resolve(process.cwd(), 'swagger.yaml')
+const swaggerDocument = YAML.load(swaggerPath)
 const app = express()
 
 // app.use(cors(configureCors()))
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  }),
+)
+app.use(json())
+
+app.use(helmet())
+app.use(cors())
+// app.use(xss())
 app.use(
   express.urlencoded({
     extended: true,
@@ -17,6 +46,18 @@ app.use(
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(
+      '<h1>Hello Vishal</h1> <h2>MarketPlace API</h2><a href="/api-docs">Documentation</a>',
+    )
+})
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.use('/v1/user', user)
 app.use('/v1/shop', shop)
 app.use('/v1/product', product)
